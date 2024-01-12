99d3ca44c7591bdb6d3b8df9aeed0ce2ad1a9986
anas20023 <yorder07@gmail.com>
2024-01-12 12:49:18 +0600

    Add print_n_1 function to C. Print from N to 1.cpp and update .gitignore

--- C. Print from N to 1.cpp
+++ C. Print from N to 1.cpp
@@ -1,8 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
+void print_n_1(int n)
+{
+    if (n == 0)
+        return;
+    cout << n;
+    print_n_1(n - 1);
+}
 int main()
 {
     int n;
     cin >> n;
+    print_n_1(n);
     return 0;
 }
