0b12586661b6df4ff96ff33c076b97f58a97071e
anas20023 <yorder07@gmail.com>
2024-01-19 09:48:10 +0600

    Q solved !!

--- Q. 3n + 1 sequence.cpp
+++ Q. 3n + 1 sequence.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
+int len = 1;
+void Set_len(int num)
+{
+    if (num == 1)
+    {
+        return;
+    }
 
+    if (num % 2 != 0)
+    {
+        len++;
+        Set_len((3 * num) + 1);
+    }
+    else
+    {
+        len++;
+        Set_len(num / 2);
+    }
+}
 int main()
 {
+    int x;
+    cin >> x;
+    if (x == 1)
+    {
+        cout << x << "\n";
+        return 0;
+    }
+    else if (x == 2)
+    {
+        cout << x << "\n";
+        return 0;
+    }
+    Set_len(x);
+    cout << len << "\n";
     return 0;
 }
